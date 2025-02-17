23caae4679ba885a385acb5d75255300d2ea17ba
parkjihun22 <eyeful31@naver.com>
2025-02-17 12:29:11 +0900

    feat : 클릭안됨 수정

--- src/components/FixIcon/FixIcon.jsx
+++ src/components/FixIcon/FixIcon.jsx
@@ -65,7 +65,6 @@ const FixIcon = ({ type }) => {
           {IconContent()}
         </>
       ) : (
-        <div>
           <div className={styles.buttomBtnContainer}>
             {/* 방문예약 a 태그를 클릭하면 팝업이 열리도록 처리 */}
             <a
@@ -88,7 +87,6 @@ const FixIcon = ({ type }) => {
               <PiPhoneCallFill size={25} />
               <div>전화상담</div>
             </a>
-          </div>
           {/* 관심고객 등록 팝업 렌더링 */}
           {isInterestPopupOpen && (
             <InterestPopup
